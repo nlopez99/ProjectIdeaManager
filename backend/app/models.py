cdd3220ca54600054456b81b7f23ad6c92b9187d
Nino Lopez <simplehocketcontent@gmail.com>
2020-08-22 18:27:41 -0400

    Add user, project, and technology models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,30 @@
 from wsgi import db
+from sqlalchemy import func
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    handle = db.Column(db.String)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=func.now())
+    projects = db.relationship("Project", backref="user")
 
 
 class Project(db.Model):
     proj_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     name = db.Column(db.String)
-    technology = db.relationship("technology")
+    description = db.Column(db.String)
+    link = db.Column(db.String)
+    technologies = db.relationship("Technology", backref="project")
 
 
 class Technology(db.Model):
+    tech_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    purpose = db.Column(db.String)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.proj_id'))
+
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,10 +9,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI"
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+# Features
+# Create a Project with Title, Description, Link, To Do Steps, Technology
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+
+# TO DO:
+# TODO: Define DB Architecture
+# TODO: Build SA models
+# TODO: Create REST API
+# TODO: Build Login and Auth
+# TODO: Start Home Page View
